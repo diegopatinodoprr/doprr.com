db7d163b0e5c1c74600974c936ae08d244e9a276
d.patino <diegopatino@doprr.com>
2016-08-07 00:10:07 +0200

    mise a jour dse

--- app/doprr/DoprrFloor.js
+++ app/doprr/DoprrFloor.js
@@ -15,13 +15,12 @@ Utils();
   }
 
   function mouseMoveListener(evt) {
-
     updateLines(evt.clientX)
   }
 
   function resizeFloor() {
     Logger.log("DoprrFlor.resizeFloor()")
-    floorContainer.style.height = (Utils.getActualHeight() * 0.5)+"px";
+    floorContainer.style.height = (Utils.getActualHeight() * 0.5) + "px";
     updateLines(Utils.getActualWidth())
   }
 
@@ -38,50 +37,45 @@ Utils();
       setStyleLine(new_div, line);
       divLines.push(new_div)
       floorContainer.appendChild(new_div)
-      new_div.addEventListener("mouseover",updateSingleLine)
+      new_div.addEventListener("mouseover", updateSingleLine)
     }
   }
 
   function updateLines(sceneWidth) {
-    var start =(sceneWidth>0)?(0):(-3);
-
-    for (var line =start; line < sceneWidth+3; line++) {
-         // updateSingleLine(line, sceneWidth)
+    var start = (sceneWidth > 0) ? (0) : (-3);
+    for (var line = start; line < sceneWidth + 3; line++) {
+      // updateSingleLine(line, sceneWidth)
     }
   }
 
   function updateSingleLine(evt) {
-    var d=evt.target
-    TweenMax.to(d, 2, {height:getRandomHeight(),onComplete:lineAnimationEnd,onCompleteParams :[d]});
+    var d = evt.target
+    TweenMax.to(d, 2, {height: getRandomHeight(), onComplete: lineAnimationEnd, onCompleteParams: [d]});
+    /*  if (line < sceneWidth  ) {
 
-  /*  if (line < sceneWidth  ) {
-
-      TweenMax.to(divLines[line], 2, {height:getRandomHeight(),onComplete:lineAnimationEnd,onCompleteParams :[divLines[line]]});
-    } else {
-      TweenMax.to(divLines[line], 2, {height:"5px",onComplete:lineAnimationEnd,onCompleteParams :[divLines[line]]});
-    }*/
+     TweenMax.to(divLines[line], 2, {height:getRandomHeight(),onComplete:lineAnimationEnd,onCompleteParams :[divLines[line]]});
+     } else {
+     TweenMax.to(divLines[line], 2, {height:"5px",onComplete:lineAnimationEnd,onCompleteParams :[divLines[line]]});
+     }*/
   }
-  function lineAnimationEnd(line)
-  {
-    TweenMax.to(line, 5, {height:getRandomHeight()});
+
+  function lineAnimationEnd(line) {
+    TweenMax.to(line, 5, {height: getRandomHeight()});
   }
 
   function setStyleLine(lineDivElement, left) {
-    lineDivElement.style.display = "block"
-    lineDivElement.style.width = "1px"
+    // lineDivElement.style.display = "block"
+    // lineDivElement.style.width = "1px"
     lineDivElement.style.height = getRandomHeight()
-    lineDivElement.style.overflow = "hidden"
+    //  lineDivElement.style.overflow = "hidden"
     lineDivElement.style.left = (left) + "px"
-    lineDivElement.style.backgroundColor = "#c1c1c1"
-    lineDivElement.style.position = "absolute"
-    lineDivElement.style.bottom = 0
-
-
+    // lineDivElement.style.backgroundColor = "#c1c1c1"
+    ///lineDivElement.style.position = "absolute"
+    //.style.bottom = 0
   }
-  function getRandomHeight(){
-    return ((Math.random()*Utils.getActualHeight())+10)+"px"
 
-
+  function getRandomHeight() {
+    return ((Math.random() * Utils.getActualHeight()) + 10) + "px"
   }
 
   init()
