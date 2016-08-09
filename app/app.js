025b76b67383b2f9534c95aa1a37de414719b610
d.patino <diegopatino@doprr.com>
2016-08-09 23:05:49 +0200

    supression de myApp par doprr, supresion de view1 par home,ca ne march epas pour le moment

--- app/app.js
+++ app/app.js
@@ -1,14 +1,27 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('doprr', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'doprr.home',
+  'doprr.view2',
+  'doprr.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]).controller('doprrHomeController',function($scope)
+{
+
+}
+).controller("faceView",function($scope)
+{
+
+  $scope.face={
+    getFaceTemplateHtml:function(){
+
+      return "faceView/FaceView.html";
+    }
+  }
+});
