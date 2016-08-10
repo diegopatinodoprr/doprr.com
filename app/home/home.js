ab99c9ae6ed2f231aef84febbde65eb06c70dad6
d.patino <diegopatino@doprr.com>
2016-08-10 23:24:51 +0200

    mise a jour boottrap

--- app/home/home.js
+++ app/home/home.js
@@ -1,14 +1,14 @@
 'use strict';
-
 angular.module('doprr.home', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'home/home.html',
-    controller: 'View1Ctrl'
-  });
-}])
-
-.controller('View1Ctrl', [function() {
-
-}]);
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {
+      templateUrl: 'home/home.html',
+      controller: 'homeCtrl'
+    });
+  }])
+  .controller('homeCtrl', ['$scope', function ($scope) {
+    console.log('hometttt', $scope)
+    $scope.doprrOpen = function (dataId) {
+      console.log("open : ", dataId)
+    }
+  }]);
