80c8f15e71315fb451e2711d269b812bad2ac46e
diego.patino <diego.patino@photoweb.fr>
2021-01-17 11:20:57 +0100

    Fix Display in home page

--- src/app/doprr-body/doprr-body.component.ts
+++ src/app/doprr-body/doprr-body.component.ts
@@ -16,23 +16,14 @@ import {
   templateUrl: './doprr-body.component.html',
   styleUrls: ['./doprr-body.component.css'],
   animations: [
-    trigger('appearsToUpLeftSide', [
-      transition(':enter', [
-        query('@appearsToUpLeftSide', style({ transform: 'translateY(-100px)', opacity: 0 }), { optional: true }),
-        query('@appearsToUpLeftSide', style({ transform: 'translateX(-200px)' }), { optional: true }),
-        query('@appearsToUpLeftSide', [
-          stagger(50, [
-            animate('1500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ transform: 'none', opacity: 1 }))])
-        ], { optional: true })
-      ])
-    ]),
+
     trigger('bodyContens', [
       transition(':enter', [
         query('@bodyContens', style({ width: 0, height: 0 }), { optional: true }),
         query('@bodyContens', [
           animate('1500ms cubic-bezier(0.35, 0, 0.25, 1)', style(
             {
-              width: 'calc(100% - 150px)', height: 'calc(100vh - 150px)'
+              width: 'calc(100% - 4px)', height: 'calc(100vh - 2px)'
             }))], { optional: true }),
       ])
     ])
@@ -41,7 +32,6 @@ import {
 })
 export class DoprrBodyComponent implements OnInit, AfterViewInit {
   containers = ['contentInside1', 'contentInside2', 'contentInside3', 'contentInside4', 'contentInside5'];
-  @HostBinding('@appearsToUpLeftSide')
   @HostBinding('@bodyContens')
   enabled = 0;
   constructor() { }
